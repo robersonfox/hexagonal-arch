9e5f848f79361416ef5a92382937674cb883f19b
Roberson Carlos <robersonfox@gmail.com>
2022-08-06 22:21:16 -0300

    Erro no generationType do Heroku

--- src/main/java/br/dev/dreamdigital/usuarios/entities/User.java
+++ src/main/java/br/dev/dreamdigital/usuarios/entities/User.java
@@ -6,12 +6,15 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,7 +29,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = { "email" }) })
 public class User implements Serializable {
     @Id
-    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     @Column(updatable = false, nullable = false)
     private Long id;
 
